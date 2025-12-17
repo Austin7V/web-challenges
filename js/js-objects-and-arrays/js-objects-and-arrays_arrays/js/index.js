2f63b69259f42f77844d9180b90325d6efdc3943
Sergey Badin <sergeykfg1@me.com>
2025-12-17 11:01:28 +0100

    add new element to array using push method

--- js/js-objects-and-arrays/js-objects-and-arrays_arrays/js/index.js
+++ js/js-objects-and-arrays/js-objects-and-arrays_arrays/js/index.js
@@ -38,6 +38,7 @@ const nestedNumber = nestedNumbers[1][2][0];
 
 const petsWithPush = ["dog", "cat", "rabbit"];
 
+petsWithPush.push("rat");
 // EXERCISE 7
 // Use the `.pop()` method to remove the last item from `fruitsWithPop`.
 
