01c0a297ecacff76e87133ff0295c7c0c4b0c37c
Sergey Badin <sergeykfg1@me.com>
2026-01-06 15:43:52 +0100

    Add toast message create and clear functionality

--- js/js-create-element/js-createelement_toast-messages/js/index.js
+++ js/js-create-element/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,12 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const toastMessage = document.createElement("li");
+  toastMessage.classList.add("toast-container__message");
+  toastMessage.textContent = "This is a toast message!";
+  toastContainer.appendChild(toastMessage);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
