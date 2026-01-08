dde3df61267662ca2df0133feac2d4fc697c34ef
Sergey Badin <sergeykfg1@me.com>
2026-01-08 16:27:45 +0100

    Log character name and eye_color from SWAPI response

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -16,8 +16,10 @@ fetchData(url); */
 async function fetchData(url) {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.results[2].eye_color);
-  console.log("Fetch test...");
+  /* console.log(data.results[2].eye_color); */
+  const character = data.results[2];
+  console.log("name:", character.name, "eye_color:", character.eye_color);
+  /*   console.log("Fetch test..."); */
 }
 
 fetchData(url);
