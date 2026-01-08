2ccd9948a3ec2713417237735fd133dc0e522259
Sergey Badin <sergeykfg1@me.com>
2026-01-08 16:09:24 +0100

    Refactor SWAPI fetch to async/await and log R2-D2 eye_color

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,12 +2,21 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData(url) {
+/* function fetchData(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.results[2].eye_color);
     });
+  console.log("Fetch test...");
+}
+
+fetchData(url); */
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.results[2].eye_color);
   console.log("Fetch test...");
 }
 
