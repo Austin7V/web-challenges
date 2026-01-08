44879efe30ce25ea90e85c797ce183264ac5f62e
Sergey Badin <sergeykfg1@me.com>
2026-01-08 16:39:45 +0100

    Fetch SWAPI people with async/await and log results

--- js/js-fetch/js-fetch_star-wars/js/index.js
+++ js/js-fetch/js-fetch_star-wars/js/index.js
@@ -3,6 +3,19 @@ import { renderElement } from "./utils.js";
 
 console.clear();
 
+const url = "https://swapi.py4e.com/api/people";
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+
+  console.log("Fetch test..");
+}
+
+fetchData(url);
+
 const EXAMPLE_DATA = {
   name: "Luke Skywalker",
   height: "172",
