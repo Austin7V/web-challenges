fead661d6af4c22f0d1dc7ca7a7fc4f052c3e6f0
Sergey Badin <sergeykfg1@me.com>
2026-01-08 15:46:49 +0100

    add fetch function

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,13 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+function fetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  console.log("Fetch request sent...");
+}
 
-fetchData();
+fetchData(url);
