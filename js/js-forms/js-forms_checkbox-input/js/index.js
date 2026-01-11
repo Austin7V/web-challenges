3fac69e5996d65e26b929018928d81670612be2e
Sergey Badin <sergeykfg1@me.com>
2026-01-11 19:57:46 +0100

    Validate TOS checkbox and toggle error and success messages

--- js/js-forms/js-forms_checkbox-input/js/index.js
+++ js/js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -11,12 +12,36 @@ function hideTosError() {
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
+function hideSuccess() {
+  success.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  success.removeAttribute("hidden");
+}
+
+hideTosError();
+hideSuccess();
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  hideSuccess();
   // --v-- write your code here --v--
-
+  if (!tosCheckbox.checked) {
+    showTosError();
+    return;
+  }
+  hideTosError();
+  showSuccess();
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
