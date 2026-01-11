c1e296e2df5395e177d7199d2d731418c37d7a55
Sergey Badin <sergeykfg1@me.com>
2026-01-11 19:27:44 +0100

    Handle form submit with simple input access and calculate age-badness sum

--- js/js-forms/js-forms_handle-form-submit/js/index.js
+++ js/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,31 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+const firstNameInput = form.elements.firstName;
+const ageInput = form.elements.age;
+const badnessInput = form.elements.badness;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstName = firstNameInput.value;
+  const age = ageInput.value;
+  const badness = badnessInput.value;
+
+  const data = {
+    firstName: firstName,
+    age: age,
+    badness: badness,
+  };
+
+  console.log(data);
+
+  const ageBadnessSum = Number(age) + Number(badness);
+  console.log(
+    `The age-badness-sum of "${data.firstName}" is "${ageBadnessSum}"`
+  );
+
+  form.reset();
+  firstNameInput.focus();
+});
