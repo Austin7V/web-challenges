c301f66a802d82d2788853ceef68e003136f84d5
Sergey Badin <sergeykfg1@me.com>
2026-01-15 16:56:50 +0100

    implement Article component with heading, form elements, and link

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Welcome to React!</h2>
+      <label htmlFor="react-input">React Input</label>
+      <input id="react-input" type="text" />
+      <a className="article__link" href="https://react.dev/">
+        Learn more about React
+      </a>
+    </article>
+  );
+}
+
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article />;
 }
