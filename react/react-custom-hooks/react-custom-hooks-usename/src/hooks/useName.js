56f791eadf387ba77b631493f8edf5b0087746d8
Sergey Badin <sergeykfg1@me.com>
2026-01-23 16:45:42 +0100

    Add useName custom hook and connect it to inputs in App

--- react/react-custom-hooks/react-custom-hooks-usename/src/hooks/useName.js
+++ react/react-custom-hooks/react-custom-hooks-usename/src/hooks/useName.js
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
 export default function useName() {
-  return [null, null, null];
+  //Zuerst Vorname speichern als eigene useState, weil er über ein Input feld ändern soll.
+  const [firstName, setFirstName] = useState("");
+  // die selber so wie mit Vorname wars..
+  const [lastName, setLastName] = useState("");
+  // Volle Name hat kein eigener useState, sondern wird aus Vor und Nachname erstellt, dadurch vermeiden wird doppelte useState.
+
+  const fullName = `${firstName} ${lastName}`.trim();
+  // Ich muss ein Array zurück geben.
+  return [fullName, setFirstName, setLastName];
 }
